13091c1ba90a1f35db9334bacd4d1fab30626cbc
Mircea667 <74052150+Mircea667@users.noreply.github.com>
2020-11-18 14:18:33 +0100

    Add files via upload

--- FAKE_NEWS.py
+++ FAKE_NEWS.py
@@ -14,12 +14,11 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
-from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split 
 from nltk.corpus import stopwords
-
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import learning_curve
 
 
 # Cargar CSV
@@ -40,6 +39,8 @@ print((df.columns))
 
 X = df['title'].astype(str) + ' ' + df['body'].astype(str)
 y = df['Category']
+
+
 #y_list=y.tolist()
 
 # Dividir los datos 70% train 30% test
@@ -84,6 +85,105 @@ pred_RF=random_forest.predict(X_test2)
 score1 = metrics.accuracy_score(y_test,pred_NB )
 score2 = metrics.accuracy_score(y_test,pred_DT)
 score3 = metrics.accuracy_score(y_test,pred_RF)
+
+##LEARNING CURVES
+
+def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None,
+                        n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 5)):
+
+    if axes is None:
+        _, axes = plt.subplots(1, 3, figsize=(20, 5))
+
+    axes[0].set_title(title)
+    if ylim is not None:
+        axes[0].set_ylim(*ylim)
+    axes[0].set_xlabel("Training examples")
+    axes[0].set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores, fit_times, _ = \
+        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
+                       train_sizes=train_sizes,
+                       return_times=True)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1)
+
+    # Plot learning curve
+    axes[0].grid()
+    axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+    axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1,
+                         color="g")
+    axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+    axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Cross-validation score")
+    axes[0].legend(loc="best")
+
+    # Plot n_samples vs fit_times
+    axes[1].grid()
+    axes[1].plot(train_sizes, fit_times_mean, 'o-')
+    axes[1].fill_between(train_sizes, fit_times_mean - fit_times_std,
+                         fit_times_mean + fit_times_std, alpha=0.1)
+    axes[1].set_xlabel("Training examples")
+    axes[1].set_ylabel("fit_times")
+    axes[1].set_title("Scalability of the model")
+
+    # Plot fit_time vs score
+    axes[2].grid()
+    axes[2].plot(fit_times_mean, test_scores_mean, 'o-')
+    axes[2].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1)
+    axes[2].set_xlabel("fit_times")
+    axes[2].set_ylabel("Score")
+    axes[2].set_title("Performance of the model")
+
+    return plt
+
+
+fig, axes = plt.subplots(3, 1,figsize=(10, 15))
+
+#X, y = load_digits(return_X_y=True)
+
+title = "Learning Curves (Naive Bayes)"
+# Cross validation with 100 iterations to get smoother mean test and train
+# score curves, each time with 20% data randomly selected as a validation set.
+cv = ShuffleSplit(n_splits=100, test_size=0.3, random_state=0)
+
+estimator1 = GaussianNB()
+plot_learning_curve(estimator1, title, X, y,
+                    cv=cv, n_jobs=-1)
+
+title2 = r"Learning Curves (DT)"
+# SVC is more expensive so we do a lower number of CV iterations:
+cv = ShuffleSplit(n_splits=100, test_size=0.3, random_state=0)
+
+estimator2 = DecissionTreeClassifier()
+plot_learning_curve(estimator2, title2, X, y, 
+                    cv=cv, n_jobs=-1)
+
+title3 = r"Learning Curves (RF)"
+# SVC is more expensive so we do a lower number of CV iterations:
+cv = ShuffleSplit(n_splits=100, test_size=0.3, random_state=0)
+
+estimator3 = RandomForestClassifier()
+plot_learning_curve(estimator2, title3, X, y,
+                    cv=cv, n_jobs=-1)
+plt.show()
+
+
+
+#lc1=learning_curve(GaussianNB(), X_train2, y_train)
+#lc1.show()
+#lc2=learning_curve(DecisionTreeClassifier(), X_train2, y_train)
+#lc2.show()
+#lc3=learning_curve(RandomForestClassifier(), X_train2, y_train )
+#lc3.show()
 #MATRIZ DE CONFUSION DEL NAIVE BAYES
 print("accuracy:   %0.3f" % score1)
 
@@ -117,6 +217,3 @@ print(metrics.confusion_matrix(y_test,pred_RF))
 print('------------------------------')
 
 
-# Entrenar los modelos con X_train e y_train
-
-# Evaluar los modelos usando X_test e y_test
